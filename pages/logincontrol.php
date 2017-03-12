fe97d40040f6e9aed43c86080482b6ca32d99e0a
dreadlock60 <dali95brahem@gmail.com>
2017-03-12 09:52:23 +0100

    ddaEQd vsdfz

--- pages/logincontrol.php
+++ pages/logincontrol.php
@@ -8,9 +8,7 @@ if(isset($_SESSION['log']))
 		 <li class="nav-item active">
 			 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
 		 </li>
-		 <li class="nav-item">
-			 <a class="nav-link" href="#">Contact Us</a>
-		 </li>
+		 
 		 <li class="nav-item dropdown">
 			 <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
 			 <div class="dropdown-menu" aria-labelledby="dropdown01">
