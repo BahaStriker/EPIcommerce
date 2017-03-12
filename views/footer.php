2498291edd772c442988d386b6e7fdea091a4412
dreadlock60 <dali95brahem@gmail.com>
2017-03-12 04:30:49 +0100

    ddddd

--- views/footer.php
+++ views/footer.php
@@ -50,12 +50,18 @@ $('#exampleModal').on('show.bs.modal', function (event) {
  <script type="text/javascript">
  var $search = $('.sear');
  $(".btnn").click( function (h) {
+$("search").toggle();
 
-$search.css('width', '250px');
-
-document.getElementsByName('search')[0].placeholder='Search';
-
-});
+})
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
+});;
  </script>
 <footer class="bg-primary">
 <div class="container">
