372b32b190ab8867e0b8d9849ac5ea965bd409b6
Baha Striker <bahastriker@outlook.com>
2017-03-12 06:01:35 +0100

    dunno

--- pages/search.php
+++ pages/search.php
@@ -4,17 +4,24 @@ if(isset($_POST['search'])
 	$ITEM	=	trim($_POST['item']);
 	$ITEM	=	office_secure($ITEM);
 	
-	$Query	=	$mysqli->query("SELECT * FROM `product` WHERE `Name` LIKE '%{$ITEM}%'");
-	
-	if($Query->num_rows == 0)
+	if(empty($ITEM))
 	{
-		$SEARCHRESULTS	=	"<div class='alert alert-danger'>No searching results found.</div>";
+		$SEARCHRESULTS	=	"<div class='alert alert-danger'>Search field cannot be empty.</div>";
 	}
 	else
 	{
-		while($Results	=	$Query->fetch_array())
+		$Query	=	$mysqli->query("SELECT * FROM `product` WHERE `Name` LIKE '%{$ITEM}%'");
+		
+		if($Query->num_rows == 0)
 		{
-			$SEARCHRESULTS	.=	"<a href='#'>".$Results['Name']."</a></br>";
+			$SEARCHRESULTS	=	"<div class='alert alert-danger'>No searching results found.</div>";
+		}
+		else
+		{
+			while($Results	=	$Query->fetch_array())
+			{
+				$SEARCHRESULTS	.=	"<a href='#'>".$Results['Name']."</a></br>";
+			}
 		}
 	}
 }
