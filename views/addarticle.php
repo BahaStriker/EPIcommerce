35955c294ef7121138c7b63b32251812c9fb93c3
Baha Striker <bahastriker@outlook.com>
2017-03-12 07:29:40 +0100

    fixed sellers

--- views/addarticle.php
+++ views/addarticle.php
@@ -1,3 +1,7 @@
+<?php
+if ($SESSION['role'] == 'seller')
+{
+?>	
 <div class="margi">
 <div class="progress">
 <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
@@ -30,3 +34,28 @@
 
 </div>
 </div>
+<?php
+}
+else
+{
+?>
+<div class="margi">
+<div class="progress">
+<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
+</div>
+</div>
+<div class="topi">
+</div>
+<div class="container">
+<div class="row">
+
+<div class="col-md-6 col-sm-6 col-xs-6">
+
+Forbidden for members!
+</div>
+
+</div>
+</div>
+<?php	
+}
+?>
